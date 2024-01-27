bbbb1dc4c60eb71a7bfb8dce1fe193597d93af3f
Minhaj28 <mdminhajuddin.official@gmail.com>
2024-01-27 23:19:34 +0600

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    basePath: '/app',
-    assetPrefix: '/app/',
     output:"export",
+    appDir: true,
   },
 }
 
